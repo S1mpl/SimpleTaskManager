f632f9eb899f6d5045f015f3e3aff9d6f8100fda
S1mply <yl@open.by>
2017-03-01 17:31:52 +0300

    Добавлена возможнасть редактировать и удалять проекты

--- Project/views.py
+++ Project/views.py
@@ -10,23 +10,35 @@ from django.db.models import Q
 from django.views.generic.base import TemplateView
 from Project.forms import ProjectCreateForm
 from datetime import datetime
-from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage
+from django.http import Http404
+from django.views.generic.edit import DeleteView
 
 
 
-class ProjectListView(PageNumberView, ListView):
-    model = Project
+class ProjectListView(PageNumberView, TemplateView):
     template_name = 'project/project_index.html'
-    paginate_by = 10
-    users = None
+    projects = None
+    project = None
+    paginator = None
 
     def get(self, request, *args, **kwargs):
-        self.users = Project.objects.filter(Q(users=request.user))
+        if request.user.is_admin:
+            self.paginator = Paginator(Project.objects.all(), 10)
+        else:
+            self.paginator = Paginator(Project.objects.filter(Q(users=request.user)), 10)
+        try:
+            self.projects = self.paginator.page(request.GET['page'])
+        except KeyError:
+            self.projects = self.paginator.page(1)
+        except EmptyPage:
+            raise Http404()
         return super(ProjectListView, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(ProjectListView, self).get_context_data(**kwargs)
-        context["users"] = self.users
+        context["projects"] = self.projects
+        context['paginator'] = self.paginator
         return context
 
 
@@ -36,7 +48,6 @@ class ProjectCreate(TemplateView):
     form = None
     manager = None
     developer = None
-    select_managers = []
 
     def get(self, request, *args, **kwargs):
         if request.user.role == "Manager" or request.user.is_admin:
@@ -64,3 +75,66 @@ class ProjectCreate(TemplateView):
         else:
             messages.add_message(request, messages.ERROR, self.form.errors)
             return redirect(reverse('project_add'))
+
+
+class ProjectUpdate(TemplateView):
+    project = None
+    template_name = 'project/project_edit.html'
+    form = None
+    manager = None
+    developer = None
+    user = None
+
+    def get(self, request, *args, **kwargs):
+        self.project = Project.objects.get(pk = self.kwargs['pk'])
+        self.user = Project.objects.filter(users=request.user)
+        if (request.user.role == "Manager" and self.user) or request.user.is_admin :
+            self.form = ProjectCreateForm(instance=self.project)
+            self.manager = User.objects.filter(role='Manager')
+            self.developer = User.objects.filter(role='Developer')
+            return super(ProjectUpdate, self).get(request, *args, **kwargs)
+        else:
+            return redirect(reverse('main'))
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectUpdate, self).get_context_data(**kwargs)
+        context['project'] = self.project
+        context['managers'] = self.manager
+        context['developers'] = self.developer
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.project = Project.objects.get(pk=self.kwargs['pk'])
+        self.user = Project.objects.filter(users=request.user)
+        if (request.user.role == "Manager" and self.user) or request.user.is_admin:
+            self.form = ProjectCreateForm(request.POST,instance=self.project)
+            if self.form.is_valid():
+                self.form.save()
+                return redirect(reverse('main'))
+            else:
+                return super(ProjectUpdate, self).get(request, *args, **kwargs)
+        else:
+            return redirect(reverse('main'))
+
+
+class ProjectDelete(DeleteView):
+    model = Project
+    template_name = 'project/project_delete.html'
+    user = None
+
+    def get(self, request, *args, **kwargs):
+        self.project = Project.objects.get(pk=self.kwargs['pk'])
+        self.user = Project.objects.filter(users=request.user)
+        if (request.user.role == "Manager" and self.user) or request.user.is_admin:
+            return super(ProjectDelete, self).get(request, *args, **kwargs)
+        else:
+            return redirect(reverse('main'))
+
+    def post(self, request, *args, **kwargs):
+        self.project = Project.objects.get(pk=self.kwargs['pk'])
+        self.user = Project.objects.filter(users=request.user)
+        if (request.user.role == "Manager" and self.user) or request.user.is_admin:
+            self.success_url = reverse('main')
+            return super(ProjectDelete, self).post(request, *args, **kwargs)
+        else:
+            return redirect(reverse('main'))
